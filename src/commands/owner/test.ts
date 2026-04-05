a9e9310e8fbe7619bba84763633e2cca343bb2e3
razvaan <razvan@uselessbrick.com>
2026-04-06 00:05:27 +0200

    sync(local_filesystem -> repo): sync repo with filesystem

--- src/commands/owner/test.ts
+++ src/commands/owner/test.ts
@@ -4,7 +4,7 @@ import { type Command } from "../../types/Command.ts";
 const preconditions = require("../../preconditions.ts");
 
 const command: Command = {
-  data: new SlashCommandBuilder().setName('test').setDescription('Literally does nothing but send a message.'),
+  data: new SlashCommandBuilder().setName('owner test').setDescription('Literally does nothing but send a message.'),
   async execute(interaction: CommandInteraction) {
     if (!await preconditions.is_owner(interaction.user.id)) {
       await interaction.reply({ content: '> You are not the owner.' });

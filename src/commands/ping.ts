2afd7fa1f401f1397d734efa861f9dbe88499af5
razvaan <razvan@uselessbrick.com>
2026-04-05 21:47:48 +0200

    sync(local_filesystem -> repo): sync repo with filesystem

--- src/commands/ping.ts
+++ src/commands/ping.ts
@@ -1,14 +1,15 @@
-import { SlashCommandBuilder, CommandInteraction, Client} from "discord.js";
+import { SlashCommandBuilder, CommandInteraction} from "discord.js";
 import { type Command } from "../types/Command.ts";
 
 const command: Command = {
   data: new SlashCommandBuilder().setName('ping').setDescription('Pong! Returns the latency of the bot.'),
   async execute(interaction: CommandInteraction) {
     const sent = await interaction.reply({ content: '> Pinging...', withResponse: true });
-    const latency = sent.client.ws.ping // horrible solution: sent.interaction.createdTimestamp - interaction.createdTimestamp;
+    const websocket_latency = sent.client.ws.ping
+    const api_latency = sent.resource?.message?.createdTimestamp as any - interaction.createdTimestamp;
 
     await interaction.editReply({
-      content: `> Pong! Latency: \`${latency}ms\``
+      content: `> Pong! \n> -# Websocket latency: \`${websocket_latency}ms\`\n> -# API latency: \`${api_latency}ms\``
     });
   },
 }

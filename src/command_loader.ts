88ea5504b04718aeff57145e78e74eeeb1ca09a6
razvaan <razvan@uselessbrick.com>
2026-04-05 19:01:25 +0200

    sync(local_filesystem -> repo): sync repo with filesystem

--- src/command_loader.ts
+++ src/command_loader.ts
@@ -1,9 +1,2 @@
-
-
 import { readdirSync } from "node:fs";
 import { join } from "node:path";
-
-const commands_dir = join(Deno.cwd(), "src", "commands");
-const commands = readdirSync(commands_dir, { withFileTypes: true })
-  .filter((dirent) => dirent.isFile())
-  .map((dirent) => dirent.name);

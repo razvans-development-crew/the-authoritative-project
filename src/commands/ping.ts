1b71b302e71d210d0e61b018c7dd4d723db2b2dc
razvaan <razvan@uselessbrick.com>
2026-04-05 21:37:56 +0200

    sync(local_filesystem -> repo): sync repo with filesystem

--- src/commands/ping.ts
+++ src/commands/ping.ts
@@ -5,16 +5,11 @@ import { type Command } from "../types/Command.ts";
 const command: Command = {
   data: new SlashCommandBuilder().setName('ping').setDescription('Pong! Returns the latency of the bot.'),
   async execute(interaction: CommandInteraction) {
-    const sent = await interaction.reply({ content: '> Pinging...', fetchReply: true });
-
-    const latency = sent.createdTimestamp - interaction.createdTimestamp;
-    const ws_ping = Math.round(client.ws.ping);
-
-    const uptime_ms = client.uptime;
-    const uptime = `${Math.floor(uptime_ms / 3600000)}h ${Math.floor((uptime_ms % 3600000) / 60000)}m ${Math.floor((uptime_ms % 60000) / 1000)}s`;
+    const sent = await interaction.reply({ content: '> Pinging...', withResponse: true });
+    const latency = sent.interaction.createdTimestamp - interaction.createdTimestamp;
 
     await interaction.editReply({
-      content: `> Pong! Latency: ${latency}ms, WS ping: ${ws_ping}ms, Uptime: ${uptime}`
+      content: `> Pong! Latency: \`${latency}ms\``
     });
   },
 }

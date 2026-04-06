b1f668525646d78cb87bc51e6cb71b2f0e8040b6
razvaan <razvan@uselessbrick.com>
2026-04-06 11:53:20 +0200

    sync(local_filesystem -> repo): sync repo with filesystem

--- src/preconditions.ts
+++ src/preconditions.ts
@@ -4,9 +4,6 @@ import { logger } from "./logging.ts";
 import { LogLevel } from "@sapphire/framework";
 
 export async function is_dc_user_id_owner(to_check: string): Promise<boolean> {
-  logger.write(LogLevel.Info, `${env_variables.get_env_variable("OWNER") == to_check}`);
-  logger.write(LogLevel.Info , `${env_variables.get_env_variable("OWNER") === to_check}`);
-
   return (await env_variables.get_env_variable("OWNER") == to_check)
 }
 

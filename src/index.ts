0ee4c920a0d241a16c7a8ee278b963c72548a7f8
razvaan <razvan@uselessbrick.com>
2026-04-06 15:47:37 +0200

    sync(local_filesystem -> repo): sync repo with filesystem

--- src/index.ts
+++ src/index.ts
@@ -8,19 +8,19 @@ const database = require("./database.ts");
 async function main() {
   await run_backend_server().catch((err) => {
     database.disconnect();
-    logger.write(LogLevel.Error, `Error running backend server ${err}`);
+    logger.write(LogLevel.Warn, `Exception while running backend server ${err}`);
   }).then(() => {
     database.connect();
     logger.write(LogLevel.Info, "Backend server has started");
   }).finally(() => {
-    logger.write(LogLevel.Info, "Backend server has stopped");
+    logger.write(LogLevel.Warn, "Backend server has stopped");
   });
 
   await run_bot().catch((err) => {
     database.disconnect();
-    logger.write(LogLevel.Error, `Error running bot ${err}`);
+    logger.write(LogLevel.Warn, `Exception while running bot ${err}`);
   }).finally(() => {
-    logger.write(LogLevel.Info, "Bot has stopped")
+    logger.write(LogLevel.Warn, "Bot has stopped")
   });
 }
 

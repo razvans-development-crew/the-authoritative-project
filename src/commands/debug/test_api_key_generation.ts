2ddd5b3451b88a1e0370b82e46dce49e9fb6944e
razvaan <razvan@uselessbrick.com>
2026-04-07 21:33:12 +0200

    sync(local_filesystem -> repo): sync repo with filesystem

--- src/commands/debug/test_api_key_generation.ts
+++ src/commands/debug/test_api_key_generation.ts
@@ -17,7 +17,19 @@ const SIGNATURE_KEY = await get_env_variable("SIGNATURE_KEY");
 export const command: Command = {
   data: new SlashCommandBuilder()
     .setName("test-api-key-generation")
-    .setDescription("Tests the API key generation."),
+    .setDescription("Tests the API key generation.")
+    .addBooleanOption(
+      o => o
+        .setName("check-as-expired")
+        .setDescription("Waits 7.6 seconds before checking the API key.")
+        .setRequired(false)
+    )
+    .addBooleanOption(
+      o => o
+        .setName("invalid-signature")
+        .setDescription("Checks the API key with an invalid signature.")
+        .setRequired(false)
+    ),
   async execute(interaction: ChatInputCommandInteraction) {
     if (!interaction.deferred) {
       await interaction.deferReply({flags: "Ephemeral"});
@@ -28,8 +40,21 @@ export const command: Command = {
       return;
     }
 
+    const check_as_expired = interaction.options.getBoolean("check-as-expired") ?? false;
+    const invalid_signature = interaction.options.getBoolean("invalid-signature") ?? false;
+
     const key = Date.now() + ":" + SECRET_KEY;
-    const signature = await sign(key, SIGNATURE_KEY);
+
+    if (check_as_expired === true) { await new Promise(r => setTimeout(r, 7600)); }
+
+    let signature;
+
+    if (invalid_signature === true) {
+      signature = await sign(key, "invalid");
+    } else {
+      signature = await sign(key, SIGNATURE_KEY);
+    }
+
     const signed_key = key + ":" + signature;
     const encrypted_key = await encrypt(signed_key, AES_ENCRYPTION_KEY);
 

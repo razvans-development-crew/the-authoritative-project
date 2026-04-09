5a67ff4782bc28f81990aaf8833ca92f06fb57c4
razvaan <razvan@uselessbrick.com>
2026-04-09 20:52:20 +0200

    sync(local_filesystem -> repo): sync repo with filesystem

--- src/commands/loader/generate_key.ts
+++ src/commands/loader/generate_key.ts
@@ -53,7 +53,7 @@ export const command: Command = {
     } else if (key_type == "2") {
       await interaction.followUp("> ```lua\n> -- This key expires in 25 seconds.\n> game:GetService(\"AssetService\"):LoadAssetAsync(" + await get_env_variable("LOADER_ASSET_ID") + ")(\'" + whitelist_info.rx_user_name + "\', \'" + key.loader_key + "\')\n> ```");
     } else if (key_type == "1") {
-      await interaction.followUp("> ```lua\n> -- This key expires in 25 seconds.\n> loadstring(game:GetService(\"HttpService\"):RequestAsync({Method = \"GET\", Url = \"" + await get_env_variable("API_URL") + "api/v3/loader\", Headers = {[\"X-Loader-Key\"] = \'" + key.loader_key + "\'}}))(\'" + whitelist_info.rx_user_name + "\', \'" + key.loader_key + "\')\n> ```");
+      await interaction.followUp("> ```lua\n> -- This key expires in 25 seconds.\n> loadstring(game:GetService(\"HttpService\"):RequestAsync({Method = \"GET\", Url = \"" + await get_env_variable("API_URL") + "api/v3/loader\", Headers = {[\"X-Loader-Key\"] = \'" + key.loader_key + "\'}}).Body)(\'" + whitelist_info.rx_user_name + "\', \'" + key.loader_key + "\')\n> ```");
     }
   }
 }

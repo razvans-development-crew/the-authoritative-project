5ba84402b9317d9dbfcc98742dbf3cd423bd0ed3
razvaan <razvan@uselessbrick.com>
2026-04-10 22:17:21 +0200

    sync(local_filesystem -> repo): sync repo with filesystem

--- src/services/gateway_reachability.ts
+++ src/services/gateway_reachability.ts
@@ -3,12 +3,19 @@ import { logger } from "../utilities/logging.ts";
 import { LogLevel } from "@sapphire/framework";
 
 export async function run_service(): Promise<void> {
-  logger.write(LogLevel.Info, "Checking if Discord's gateway is reachable...");
+  logger.write(LogLevel.Info, "Checking if Discord's gateway at gateway.discord.gg is reachable...");
 
   https.get("https://gateway.discord.gg", (res) => {
-    logger.write(LogLevel.Info, "Discord's gateway is reachable.");
+    logger.write(LogLevel.Info, "Discord's gateway at gateway.discord.gg is reachable, status code: " + res.statusCode);
   }).on("error", (err) => {
-    logger.write(LogLevel.Error, "Discord's gateway is not reachable.");
+    logger.write(LogLevel.Error, "Discord's gateway at gateway.discord.gg is not reachable.");
+    logger.write(LogLevel.Error, err);
+  });
+
+  https.get("https://discord.com/api/v10/gateway", (res) => {
+    logger.write(LogLevel.Info, "Discord's gateway at discord.com/api/v10/gateway is reachable, status code: " + res.statusCode);
+  }).on("error", (err) => {
+    logger.write(LogLevel.Error, "Discord's gateway at discord.com/api/v10/gateway is not reachable.");
     logger.write(LogLevel.Error, err);
   });
 }

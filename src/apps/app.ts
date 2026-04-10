c20cbb885fa4db80c7b3a643a98d4bc8f75f8d58
razvaan <razvan@uselessbrick.com>
2026-04-10 02:15:19 +0200

    sync(local_filesystem -> repo): sync repo with filesystem

--- src/apps/app.ts
+++ src/apps/app.ts
@@ -4,7 +4,7 @@ import { logger } from "@bogeychan/elysia-logger";
 import { join } from "path";
 import { logger as utils_logger } from "../utilities/logging.ts";
 import { LogLevel } from "@sapphire/framework";
-import { get_client_ip } from "../utilities/helpers.ts";
+// import { get_client_ip } from "../utilities/helpers.ts";
 
 export const AHEAD_OF_TIME = true;
 export const PRECOMPILE = true;
@@ -16,32 +16,33 @@ const app = new Elysia({
   nativeStaticResponse: true,
 });
 
-await load_routes(app, join(import.meta.dir, "../routes"));
 
 export async function run_backend_server(): Promise<void> {
   utils_logger.write(LogLevel.Info, "Starting backend server...");
 
-  // app.use(logger({
-  //   level: "debug",
-  //   formatters: {
-  //     bindings: () => ({ pid: null })
-  //   },
-  //   transport: {
-  //     target: "pino-pretty",
-  //     options: {
-  //       colorize: true
-  //     }
-  //   },
-  //   autoLogging: true
-  // }));
+  await load_routes(app, join(import.meta.dir, "../routes"));
 
-  app.onAfterResponse(async (context) => {
-    utils_logger.write(LogLevel.Info, `(Outgoing Response) ${await get_client_ip(context.request, context.server)} | (${context.request.method}) ${context.request.url} - ${context.set.status}`);
-  });
+  app.use(logger({
+    level: "debug",
+    formatters: {
+      bindings: () => ({ pid: null })
+    },
+    transport: {
+      target: "pino-pretty",
+      options: {
+        colorize: true
+      }
+    },
+    autoLogging: true
+  }));
 
-  app.onRequest(async (context) => {
-    utils_logger.write(LogLevel.Info, `(Incoming) ${await get_client_ip(context.request, context.server)} | (${context.request.method}) ${context.request.url} - ${context.set.status}`);
-  });
+  // app.onAfterResponse(async (context) => {
+  //   utils_logger.write(LogLevel.Info, `(Outgoing Response) ${await get_client_ip(context.request, context.server)} | (${context.request.method}) ${context.request.url} - ${context.set.status}`);
+  // });
+
+  // app.onRequest(async (context) => {
+  //   utils_logger.write(LogLevel.Info, `(Incoming) ${await get_client_ip(context.request, context.server)} | (${context.request.method}) ${context.request.url} - ${context.set.status}`);
+  // });
 
   app.listen(await env_variables.get_env_variable("PORT"), async () => {
     utils_logger.info(`Server started on port ${await env_variables.get_env_variable("PORT")} | https://127.0.0.1:${await env_variables.get_env_variable("PORT")}`);

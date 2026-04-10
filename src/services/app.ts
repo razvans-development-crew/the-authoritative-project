585f05c873073e20f44a896080f7f26b63642ab5
razvaan <razvan@uselessbrick.com>
2026-04-10 23:47:01 +0200

    sync(local_filesystem -> repo): sync repo with filesystem

--- src/services/app.ts
+++ src/services/app.ts
@@ -1,12 +1,11 @@
 import { Elysia } from "elysia";
 import { load_routes } from "../loaders/route_loader.ts";
-// import { logger } from "@bogeychan/elysia-logger";
 import { join } from "path";
 import { logger as utils_logger } from "../utilities/logging.ts";
 import { LogLevel } from "@sapphire/framework";
-// import { get_client_ip } from "../utilities/helpers.ts";
-import { staticPlugin } from "@elysiajs/static"
-import logixlysia from "logixlysia"
+import { get_client_ip } from "../utilities/helpers.ts";
+import { staticPlugin } from "@elysiajs/static";
+import { serverTiming } from "@elysiajs/server-timing";
 
 export const AHEAD_OF_TIME = true;
 export const PRECOMPILE = true;
@@ -26,33 +25,13 @@ export async function run_service(): Promise<void> {
   app.use(staticPlugin({
     assets: "static",
     prefix: "/static",
-  }))
+  }));
 
-  app.use(
-    logixlysia({
-      config: {
-        service: "tap-server",
-        showStartupMessage: true,
-        startupMessageFormat: 'banner',
-        showContextTree: true,
-        contextDepth: 2,
-        slowThreshold: 500,
-        verySlowThreshold: 1000,
-        timestamp: {
-          translateTime: "yyyy-mm-dd HH:MM:ss.SSS"
-        },
-        ip: true
-      }
-    })
-  )
+  app.use(serverTiming());
 
-  // app.onAfterResponse(async (context) => {
-  //   utils_logger.write(LogLevel.Info, `${await get_client_ip(context.request, context.server)} | (${context.request.method}) ${context.request.url} - ${context.set.status}`);
-  // });
-
-  // app.onRequest(async (context) => {
-  //   utils_logger.write(LogLevel.Info, `(Incoming) ${await get_client_ip(context.request, context.server)} | (${context.request.method}) ${context.request.url} - ${context.set.status}`);
-  // });
+  app.onAfterResponse(async (context) => {
+    utils_logger.write(LogLevel.Info, `${await get_client_ip(context.request, context.server)} | (${context.request.method}) ${context.request.url} - ${context.set.status}`);
+  });
 
   app.listen(Number(await env_variables.get_env_variable("PORT")) ?? 8000, async () => {
     utils_logger.info(`Server started on port ${await env_variables.get_env_variable("PORT")} | https://127.0.0.1:${await env_variables.get_env_variable("PORT")}`);
